d228317ed30a066ede5e4e41d4ed549e47ce9e1e
priyajoshi90 <priya.apr9@gmail.com>
2014-08-06 21:37:03 -0700

    solr and simplecov implemented

---
+++ xpress/spec/controllers/blogs_controller_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+#require 'mocha/api'
+#require 'mocha/deprecation'
+#require 'ruby_debug'
+#require 'mocha/standalone'
+#require 'mocha/object'
+describe BlogsController, :type => :controller do
+	 	render_views
+	 	fixtures :blogs
+	 	it "should redirect to index with a notice on successful save" do 
+	 		session[:blogger_id] = blogs(:one).blogger_id 
+	 		#get :index
+	 		#blogs(:one).authenticate_blogger?.returns(true)	
+	 		post 'create'
+	 		#flash[:notice].should_not be_nil
+	 		response.should redirect_to(blog_path)	
+	 	end
+=begin	 	it "should re-render new template on failed save" do 
+	 		Blog.any_instance.stubs(:valid?).returns(true)	
+	 		post 'create'
+	 		flash[:notice].should_not be_nil
+	 		response.should redirect_to(blog_path)
+	 	end
+=end
+end
---
+++ xpress/spec/models/detail_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+require 'detail.rb'
+describe Detail do
+  #pending "add some examples to (or delete) #{__FILE__}"
+  it "should have a name"
+end
